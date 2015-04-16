6c842bbab375671c22f76bcc81f070682781e4cd
Dablu Kumar <dabluk@thoughtworks.com>
2015-04-16 17:01:52 +0530

    Test case file for TestlistbookCommand modified

--- src/test/com/twu/biblioteca/TestListBooksCommand.java
+++ src/test/com/twu/biblioteca/TestListBooksCommand.java
@@ -1,8 +1,11 @@
 package test.com.twu.biblioteca;
 
+import com.twu.biblioteca.Book;
 import com.twu.biblioteca.Library;
 import com.twu.biblioteca.ListBooksCommand;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,12 +13,15 @@ import static org.junit.Assert.assertEquals;
  * Created by dabluk on 16/04/15.
  */
 public class TestListBooksCommand {
+    private ArrayList<Book> books;
     @Test
     public void checkListBooks(){
+
         Library library = new Library();
         library.addBooks();
         ListBooksCommand listBooksCommand = new ListBooksCommand(library);
-        listBooksCommand.execute();
-        assertEquals(4,listBooksCommand.getTotalbooks());
+        books = listBooksCommand.getBooks();
+        Book book = books.get(1);
+        assertEquals(2, book.getBookNo());
     }
 }
