a5a5a073d72d73d2ccb4004f66c0becac22919b2
Dablu Kumar <dabluk@thoughtworks.com>
2015-04-16 17:16:16 +0530

    Test file fot getBookDetail modified

--- src/test/com/twu/biblioteca/TestGetBookDetail.java
+++ src/test/com/twu/biblioteca/TestGetBookDetail.java
@@ -1,7 +1,28 @@
 package test.com.twu.biblioteca;
 
+import com.twu.biblioteca.Book;
+import com.twu.biblioteca.GetBookDetail;
+import com.twu.biblioteca.Library;
+import com.twu.biblioteca.ListBooksCommand;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by dabluk on 16/04/15.
  */
 public class TestGetBookDetail {
+    private ArrayList<Book> books;
+    @Test
+    public void testbookDetail(){
+
+        Library library = new Library();
+        library.addBooks();
+        GetBookDetail getBookDetail = new GetBookDetail(library);
+        books = getBookDetail.getBooks();
+        Book book = books.get(2);
+        assertEquals(3, book.getBookNo());
+    }
 }
