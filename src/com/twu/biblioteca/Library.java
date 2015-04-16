1a152abad385a294f4986806636eb3aa29c3b401
Dablu Kumar <dabluk@thoughtworks.com>
2015-04-16 23:16:23 +0530

    Library file modified

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
  * Created by dabluk on 14/04/15.
  */
 public class Library {
+
     private ArrayList<Book> books;
 
     public Library(){
@@ -24,12 +25,41 @@ public class Library {
     }
 
 
-    public void addBooks(){
-        books.add(new Book(1, "JAVA", "Herbert Schildt", "TMH"));
-        books.add(new Book(2, "J2EE", "Herbert Schildt", "SUN"));
-        books.add(new Book(3, "C++", "James Stourstrup", "TMH"));
-        books.add(new Book(4, "ORACLE", "Harish Gaur", "Oracle"));
+    public void addBooks(Book book){
+            books.add(book);
+//        books.add(new Book(1, "JAVA", "Herbert Schildt", "TMH"));
+//        books.add(new Book(2, "J2EE", "Herbert Schildt", "SUN"));
+//        books.add(new Book(3, "C++", "James Stourstrup", "TMH"));
+//        books.add(new Book(4, "ORACLE", "Harish Gaur", "Oracle"));
     }
 
 
+    public Book getBook(int bookNumber) throws BookNotFoundException    {
+        for (int i = 0; i<books.size(); i++){
+            if(books.get(i).getBookNo() == bookNumber){
+                return books.get(i);
+            }
+        }
+        throw new BookNotFoundException("Book with id "+ bookNumber + " not found");
+    }
+//
+//    public Book getBook(int bookNumber) throws BookNotFoundException {
+//        boolean flag = false;
+//        int i;
+//        for (i = 0; i < books.size(); i++) {
+//            if ( books.get(i).getBookNo() == bookNumber){
+//                flag = true;
+//                break;
+//
+//            }
+//        }
+//        if (flag == true){
+//            return books.get(i);
+//
+//        }
+//        else{
+//            throw new BookNotFoundException("Book not found");
+//        }
+//    }
 }
+
