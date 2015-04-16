7cea244442f1c094dc706ad794feb70c657db36a
Dablu Kumar <dabluk@thoughtworks.com>
2015-04-16 16:49:37 +0530

    Book File Modified

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -27,52 +27,5 @@ public class Book {
                 "     " + bookName  ;
     }
 
-    /**
-     * Created by dabluk on 14/04/15.
-     */
-    public static class Library {
-        private ArrayList<Book> books;
 
-        public Library(){
-
-            books = new ArrayList<Book>();
-        }
-
-        public ArrayList<Book> getBooks() {
-            return books;
-        }
-
-
-        public void addBooks(){
-            books.add(new Book(1, "JAVA", "Herbert Schildt", "TMH"));
-            books.add(new Book(2, "J2EE", "Herbert Schildt", "SUN"));
-            books.add(new Book(3, "C++", "James Stourstrup", "TMH"));
-            books.add(new Book(4, "ORACLE", "Harish Gaur", "Oracle"));
-        }
-
-
-    }
-
-    /**
-     * Created by dabluk on 15/04/15.
-     */
-    public static class ListBooksCommand implements ICommand {
-        private  ArrayList<Book> books;
-
-        public ListBooksCommand(Library app) {
-            books = new ArrayList<Book>();
-            this.books = app.getBooks();
-        }
-        @Override
-        public void execute() {
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
-            System.out.println("---------------Avilable Books------------------");
-            System.out.println("com.twu.biblioteca.Book No.                com.twu.biblioteca.Book Name");
-            System.out.println("_______                 _________");
-            for (int i = 0; i < books.size(); i++) {
-                System.out.println(books.get(i));
-            }
-        }
-    }
 }
