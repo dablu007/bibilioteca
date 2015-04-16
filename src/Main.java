85a4ea5f61631a8c24dcf76232305afa66a1ca30
dablu007 <dablu.kumar007@gmail.com>
2015-04-16 14:17:50 +0530

    Main file for execution
    Main file for execution of the app

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String args[]){
         Library library = new Library();
-
         library.addBooks();
         Display display = new Display();
         display.welcomeMessage();
