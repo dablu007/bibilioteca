b5527ec4491e9f9890aa511dad758dfd8fcfc865
Dablu Kumar <dabluk@thoughtworks.com>
2015-04-16 16:46:15 +0530

    Test file for library added

--- src/test/com/twu/biblioteca/TestLibrary.java
+++ src/test/com/twu/biblioteca/TestLibrary.java
@@ -1,7 +1,25 @@
 package test.com.twu.biblioteca;
+import com.twu.biblioteca.Book;
+import com.twu.biblioteca.Library;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by dabluk on 16/04/15.
  */
 public class TestLibrary {
+    private ArrayList<Book> books;
+    Library library;
+    Book book;
+    @Test
+    public void testLibrary(){
+        library = new Library();
+        library.addBooks();
+        books = library.getBooks();
+        book = books.get(0);
+        assertEquals(1,book.getBookNo());
+    }
 }
