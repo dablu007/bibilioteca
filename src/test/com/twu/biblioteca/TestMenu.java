e50534415e84c815c5112733ccb7c3d416fd421a
Dablu Kumar <dabluk@thoughtworks.com>
2015-04-16 16:48:28 +0530

    Test File for Menu Added

--- src/test/com/twu/biblioteca/TestMenu.java
+++ src/test/com/twu/biblioteca/TestMenu.java
@@ -3,5 +3,37 @@ package test.com.twu.biblioteca;
 /**
  * Created by dabluk on 16/04/15.
  */
+import com.twu.biblioteca.ICommand;
+import com.twu.biblioteca.Menu;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by dabluk on 16/04/15.
+ */
+
+
+class SampleCommand implements ICommand {
+
+    @Override
+    public void execute() {
+
+    }
+}
+
 public class TestMenu {
+    @Test
+    public void ShouldCreateMenu()  {
+        ICommand sampleCommand = new SampleCommand();
+
+        Menu menu = new Menu();
+        menu.addCommand(1,sampleCommand);
+        menu.addCommand(2,null);
+
+        assertEquals(menu.getCommand(1), sampleCommand);
+
+
+    }
 }
