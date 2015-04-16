1796f0a6baf03d7b20cf6122f8cd4f38beedbda9
Dablu Kumar <dabluk@thoughtworks.com>
2015-04-16 17:13:30 +0530

    Test file for book added and modified

---
+++ src/test/com/twu/biblioteca/TestBook.java
@@ -0,0 +1,17 @@
+package test.com.twu.biblioteca;
+
+import com.twu.biblioteca.Book;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by dabluk on 16/04/15.
+ */
+public class TestBook {
+    @Test
+    public void testbook(){
+        Book book = new Book(1, "JAVA", "Herbert Schildt", "TMH");
+        assertEquals(1,book.getBookNo());
+    }
+}
