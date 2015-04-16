dd1f7ba7eacbee75c12bbada32ac270132964548
Dablu Kumar <dabluk@thoughtworks.com>
2015-04-16 16:44:14 +0530

    Library File Added

---
+++ src/com/twu/biblioteca/Library.java
@@ -0,0 +1,35 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by dabluk on 16/04/15.
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Created by dabluk on 14/04/15.
+ */
+public class Library {
+    private ArrayList<Book> books;
+
+    public Library(){
+
+        books = new ArrayList<Book>();
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+
+    public void addBooks(){
+        books.add(new Book(1, "JAVA", "Herbert Schildt", "TMH"));
+        books.add(new Book(2, "J2EE", "Herbert Schildt", "SUN"));
+        books.add(new Book(3, "C++", "James Stourstrup", "TMH"));
+        books.add(new Book(4, "ORACLE", "Harish Gaur", "Oracle"));
+    }
+
+
+}
---
+++ src/test/com/twu/biblioteca/TestLibrary.java
@@ -0,0 +1,7 @@
+package test.com.twu.biblioteca;
+
+/**
+ * Created by dabluk on 16/04/15.
+ */
+public class TestLibrary {
+}
