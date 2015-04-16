30041ec42099d54831534ac56ece413238a2fc73
Dablu Kumar <dabluk@thoughtworks.com>
2015-04-16 16:47:29 +0530

    Test file for listBooksCommand Added

--- src/test/com/twu/biblioteca/TestListBooksCommand.java
+++ src/test/com/twu/biblioteca/TestListBooksCommand.java
@@ -1,7 +1,21 @@
 package test.com.twu.biblioteca;
 
+import com.twu.biblioteca.Library;
+import com.twu.biblioteca.ListBooksCommand;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by dabluk on 16/04/15.
  */
 public class TestListBooksCommand {
+    @Test
+    public void checkListBooks(){
+        Library library = new Library();
+        library.addBooks();
+        ListBooksCommand listBooksCommand = new ListBooksCommand(library);
+        listBooksCommand.execute();
+        assertEquals(4,listBooksCommand.getTotalbooks());
+    }
 }
