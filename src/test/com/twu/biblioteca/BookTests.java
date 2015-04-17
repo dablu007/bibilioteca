b7c01c74a5ffe47451ec4df973368f0f335e725c
Dablu Kumar <dabluk@thoughtworks.com>
2015-04-17 13:09:15 +0530

    Test file for Book

--- src/test/com/twu/biblioteca/BookTests.java
+++ src/test/com/twu/biblioteca/BookTests.java
@@ -4,6 +4,8 @@ import com.twu.biblioteca.Book;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by dabluk on 16/04/15.
@@ -12,11 +14,11 @@ public class BookTests {
     @Test
     public void ShouldAddaBook(){
         Book book = new Book(1, "JAVA", "Herbert Schildt", "TMH");
-        assertEquals(1,book.getBookNo());
+        assertEquals(1, book.getBookNo());
     }
-    @Test
-    public void ShouldNotThrowExceptionWhenNoBookIsAdded(){
-        Book book = new Book();
-        assertEquals(0, book.getBookNo());
-    }
+
+
+
+
+
 }
