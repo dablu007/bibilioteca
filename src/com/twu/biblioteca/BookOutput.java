aec0100619ec364a12456809929c19d050944562
Dablu Kumar <dabluk@thoughtworks.com>
2015-04-17 13:05:04 +0530

    BookOutput file added for testing the ListBooks and GetbooksCommand file

--- src/com/twu/biblioteca/BookOutput.java
+++ src/com/twu/biblioteca/BookOutput.java
@@ -3,5 +3,9 @@ package com.twu.biblioteca;
 /**
  * Created by dabluk on 17/04/15.
  */
-public class BookOutput {
+public class BookOutput implements ICanPrint {
+    @Override
+    public void write(String printbook) {
+        System.out.println(printbook);
+    }
 }
