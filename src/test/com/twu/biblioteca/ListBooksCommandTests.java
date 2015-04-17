e922d4f8a182c2f2db32a62875d85e86dd68de44
Dablu Kumar <dabluk@thoughtworks.com>
2015-04-17 13:07:07 +0530

    Test file for ListbooksCommand

--- src/test/com/twu/biblioteca/ListBooksCommandTests.java
+++ src/test/com/twu/biblioteca/ListBooksCommandTests.java
@@ -1,30 +1,40 @@
 package test.com.twu.biblioteca;
 
 import com.twu.biblioteca.Book;
+import com.twu.biblioteca.ICanPrint;
 import com.twu.biblioteca.Library;
-import com.twu.biblioteca.ListBooksCommand;
+import com.twu.biblioteca.command.ListBooksCommand;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 
 /**
  * Created by dabluk on 16/04/15.
  */
 public class ListBooksCommandTests {
-
-    private ArrayList<Book> books;
-    @Test
-    public void testBooksList(){
-
+    private ListBooksCommand listBooksCommand;
+    @Before
+    public void setup(){
         Library library = new Library();
         Book book = new Book(1, "JAVA", "Herbert Schildt", "TMH");
         library.addBooks(book);
+        listBooksCommand = new ListBooksCommand(library);
+    }
 
-        ListBooksCommand listBooksCommand = new ListBooksCommand(library);
-//        books = listBooksCommand.getBooks();
-//        Book book = books.get(1);
-//        assertEquals(2, book.getBookNo());
+
+    @Test
+    public void ShouldPrintBooksAddedToLibrary(){
+        ICanPrint outputWriterMock = mock(ICanPrint.class);
+        listBooksCommand.printbook(outputWriterMock, 0);
+        Mockito.verify(outputWriterMock,times(1)).write(String.format("%d%27s", 1, "JAVA"));
+
+
     }
 }
