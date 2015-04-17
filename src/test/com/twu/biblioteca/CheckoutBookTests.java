79aa15c8afb28586f54be44f18accc430208f20e
Dablu Kumar <dabluk@thoughtworks.com>
2015-04-17 14:44:10 +0530

    Test file for checkout added

--- src/test/com/twu/biblioteca/CheckoutBookTests.java
+++ src/test/com/twu/biblioteca/CheckoutBookTests.java
@@ -1,7 +1,31 @@
 package test.com.twu.biblioteca;
 
+import com.twu.biblioteca.Book;
+import com.twu.biblioteca.CheckoutBook;
+import com.twu.biblioteca.Library;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 /**
  * Created by dabluk on 17/04/15.
  */
 public class CheckoutBookTests {
+    private ArrayList<Book> books;
+    Library library;
+    @Before
+    public void setup(){
+        Book book  = new Book(1, "JAVA", "Herbert Schildt", "TMH");
+        library = new Library();
+        library.addBooks(book);
+
+    }
+
+    @Test
+    public void ShouldCheckoutABookIfAvilable(){
+        CheckoutBook checkoutBook = new CheckoutBook(library);
+
+    }
 }
+
